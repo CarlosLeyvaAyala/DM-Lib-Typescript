6f2e07f8e32df08a181a89ee94acbf3a1d6d4c34
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2022-06-14 15:42:58 -0500

    Add ForEachKeywordR

--- DmLib.ts
+++ DmLib.ts
@@ -7,6 +7,7 @@ import {
   Game,
   hooks,
   Input,
+  Keyword,
   ObjectReference,
   once,
   Outfit,
@@ -546,6 +547,23 @@ export namespace FormLib {
     while (i > 0) {
       i--
       f(o.getNthForm(i))
+    }
+  }
+  /** Iterates over all keywords belonging to some `Form`, from last to first.
+   *
+   * @param o - The form to iterate over.
+   * @param f - Function applied to each keyword.
+   */
+  export function ForEachKeywordR(
+    o: Form | null,
+    f: (keyword: Keyword) => void
+  ) {
+    if (!o) return
+    let i = o.getNumKeywords()
+    while (i > 0) {
+      i--
+      const k = Keyword.from(o.getNthKeyword(i))
+      if (k) f(k)
     }
   }
 
