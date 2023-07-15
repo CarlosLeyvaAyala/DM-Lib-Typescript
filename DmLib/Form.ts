2b26c06010668782a1220e13a0ace01d5f5e8e0a
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-07-15 17:56:35 -0600

    Update Form.ts

--- DmLib/Form.ts
+++ DmLib/Form.ts
@@ -16,7 +16,11 @@ import {
   Potion,
   SlotMask,
   SoulGem,
+  TESModPlatform,
+  Utility,
   Weapon,
+  WorldSpace,
+  printConsole,
 } from "skyrimPlatform"
 
 export const enum ItemType {
@@ -302,3 +306,162 @@ export function getFormEsp(form: Form | null): FormEspInfo {
 
 export const defaultUIdFmt = (espName: string, fixedFormId: number) =>
   `${espName}|0x${fixedFormId.toString(16)}`
+
+/**
+ * Iterates over all armors belonging to some `ObjectReference`, from last to first.
+ *
+ * @param o - The object reference to iterate over.
+ * @param f - Function applied to each armor.
+ */
+export function forEachArmorR(o: ObjectReference, f: (armor: Armor) => void) {
+  forEachItem(o, (i) => {
+    const a = Armor.from(i)
+    if (!a) return
+    f(a)
+  })
+}
+
+/**
+ * Iterates over all items belonging to some `ObjectReference`, from last to first.
+ * @internal
+ *
+ * @param o - The object reference to iterate over.
+ * @param f - Function applied to each item.
+ *
+ * @remarks
+ * Better use {@link forEachItem }.\
+ * That function sends a `Form`, not a `Form | null`, so you are guaranteed to
+ * have a form at the point that function gets executed.
+ */
+export function forEachItemR(
+  o: ObjectReference,
+  f: (item: Form | null) => void
+) {
+  let i = o.getNumItems()
+  while (i > 0) {
+    i--
+    f(o.getNthForm(i))
+  }
+}
+
+/**
+ * Iterates over all items belonging to some `ObjectReference`, from last to first.
+ *
+ * @param o - The object reference to iterate over.
+ * @param f - Function applied to each item.
+ */
+export function forEachItem(o: ObjectReference, f: (item: Form) => void) {
+  forEachItemR(o, (item) => {
+    if (!item) return
+    f(item)
+  })
+}
+
+/**
+ * Iterates over all items belonging to some `ObjectReference`, from last to first. Waits
+ * some time before each operation.
+ *
+ * @param o - The object reference to iterate over.
+ * @param wait - Time (seconds) to wait.
+ * @param f - Function applied to each item.
+ */
+export function forEachItemW(
+  o: ObjectReference,
+  wait: number,
+  f: (item: Form) => void
+) {
+  const A = async () => {
+    let i = o.getNumItems()
+    while (i > 0) {
+      i--
+      const item = o.getNthForm(i)
+      if (!item) return
+      f(item)
+      Utility.wait(wait)
+    }
+  }
+  A()
+}
+
+/**
+ * Creates a persistent chest hidden somewhere in Tamriel.
+ *
+ * @returns The FormId of the recently created chest. `null` if no chest could be created.
+ *
+ * @remarks
+ * This chest can be used as a permanent storage that never resets.
+ *
+ * This function expects the "Tamriel" map to exist. The most likely case is that
+ * map exists, since it's the map were all cities are located.
+ *
+ * @privateRemarks
+ * Because of the way things are created in Skyrim, we first need to spawn the chest
+ * at the player's location.
+ */
+export function createPersistentChest() {
+  // Spawn chest at player's location
+  const p = Game.getPlayer() as Actor
+  const c = p.placeAtMe(Game.getFormEx(0x70479), 1, true, false)
+  if (!c) return null
+
+  // Move the chest to Tamriel
+  const world = WorldSpace.from(Game.getFormEx(0x3c))
+  TESModPlatform.moveRefrToPosition(c, null, world, 0, 0, -10000, 0, 0, 0)
+
+  return c.getFormID()
+}
+
+/**
+ * Tries to get a persistent chest defined in some place and creates a new
+ * one if it doesn't exist.
+ *
+ * @param Getter - Function that gets an already existing chest.
+ * @param Setter - Function that saves a newly created chest.
+ * @param Logger - Function to log an error if a new chest couldn't be created.
+ *
+ * @remarks
+ * This function assumes you want to somehow store and retrieve the chest from
+ * some database (most probably, using JContainers or PapyrusUtil).
+ *
+ * This has to be done this way, since at the moment of creating this function,
+ * Skyrim Platform has no means to read and write values directly to the SKSE co-save.
+ *
+ * @example
+ * ```
+ * // This uses a JContainers database to know what chest is being created
+ * const path = "some.JContainers.path"
+ * const h = GetSomeJContainersHandle(path)
+ * const someForm = Game.getFormEx(0x14)
+ *
+ * const Getter = () => {
+ *   return JFormMap.getForm(h, someForm)
+ * }
+ * const Setter = (frm: Form | null) => {
+ *   JFormMap.setForm(h, someForm, frm)
+ *   SaveSomeJContainersHandle(h, path)
+ * }
+ *
+ * const chest = getPersistentChest(Getter, Setter, printConsole)
+ * ```
+ */
+export function getPersistentChest(
+  Getter: () => Form | null,
+  Setter: (frm: Form | null) => void,
+  Logger?: (msg: string) => void
+) {
+  let frm = Getter()
+  if (!frm) {
+    const newChest = createPersistentChest()
+    if (!newChest) {
+      const msg =
+        "Could not create a persistent chest in Tamriel. " +
+        "Are you using a mod that substantially changes the game?"
+      if (Logger) Logger(msg)
+      else printConsole(msg)
+      return null
+    }
+    frm = Game.getFormEx(newChest)
+    Setter(frm)
+  }
+  return frm
+}
